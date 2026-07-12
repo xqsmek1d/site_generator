447b8227e0f750a39ca6a43034960b7d8f2eaca2
xqsme <r.c.sonnemans@gmail.com>
2026-07-12 22:17:37 +0200

    C: made changes to align with Github Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ from markdown_to_html_node import markdown_to_html_node
 from markdown_blocks import extract_title
 import os
 
-def generate_page(from_path:str, template_path: str, dest_path: str):
+def generate_page(from_path:str, template_path: str, dest_path: str, basepath: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -13,6 +13,8 @@ def generate_page(from_path:str, template_path: str, dest_path: str):
     page_title = extract_title(markdown)
     template = template.replace("{{ Title }}", page_title)
     template = template.replace("{{ Content }}", HTML_string)
+    template = template.replace('href="/',f'href="{basepath}')
+    template = template.replace('src="/',f'src="{basepath}')
 
     # Create output directory and write result to it
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -2,9 +2,16 @@ from textnode import TextNode, TextType
 from copystatic import copystatic
 from generate_page import generate_page
 from generate_pages_recursive import generate_pages_recursive
+import sys
 
 def main():
+    if len(sys.argv) == 1:
+        basepath = "/"
+    elif len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    else:
+        raise Exception("Error: invalid argument entered")
     copystatic()
-    #generate_page("content/index.md","template.html","public/index.html")
-    generate_pages_recursive("./content","template.html","./public")
+    #generate_page("content/index.md","template.html","public/index.html",basepath)
+    generate_pages_recursive("./content","template.html","./docs",basepath)
 main()
